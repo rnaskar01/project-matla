eb307f7d7cf55c8871d2974c45f59e1f9c7c0438
Rakesh Naskar <rakeshnaskar499@gmail.com>
2026-02-20 22:51:19 +0530

    issue solved in backend

--- Backend/Server.js
+++ Backend/Server.js
@@ -104,6 +104,6 @@ app.use((req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+// app.listen(5000, () => {
+//   console.log("Server running on port 5000");
+// });
