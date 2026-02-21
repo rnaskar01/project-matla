47c0817d63939ebec3a8320a9e61062caca1890b
Rakesh Naskar <rakeshnaskar499@gmail.com>
2026-02-21 17:23:55 +0530

    isuue in gmail auth

--- Backend/Server.js
+++ Backend/Server.js
@@ -20,10 +20,15 @@ app.post("/contact", async (req, res) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false, // must be false for 587
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
+      },
+      tls: {
+        rejectUnauthorized: false,
       },
     });
 
